9c08c02b7d5b9bac131c55d797c6f14d1c585dd6
nephi-asha <nephiasha2017@gmail.com>
2025-09-03 01:20:47 +0100

    fix: Return the correct array of restock requests in getAllRestockRequests

--- controller/restocks-controller.js
+++ controller/restocks-controller.js
@@ -67,7 +67,7 @@ exports.getAllRestockRequests = async (req, res) => {
             );
             restock.items = restockItemsResult.rows;
         }
-        res.status(200).json(restock.items);
+        res.status(200).json(restocks);
     } catch (error) {
         console.error('Error fetching restock requests:', error);
         handleError(res, 500, 'Server error fetching restock requests.');
